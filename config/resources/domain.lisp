aaa935c67e9643ae6a6e2592fcc6419259d52bad
Nóra Szepes <nora.szepes@tenforce.com>
2019-04-02 13:13:12 +0200

    Updated domain.lisp

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,7 +1,5 @@
 (in-package :mu-cl-resources)
 
-(defparameter *max-group-sorted-properties* nil)
-
 (define-resource hotel ()
   :class (s-prefix "ext:Hotel")
   :properties `((:name :language-string-set ,(s-prefix "dct:title"))
